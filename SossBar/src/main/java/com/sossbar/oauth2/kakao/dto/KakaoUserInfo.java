984f772e47d4a758d4f4ab1feadfdf4b9d3da502
ttttkii913 <dbgkdud821@daum.net>
2026-03-30 03:34:02 +0900

    Feat/#2: Kakao Social Login

---
+++ SossBar/src/main/java/com/sossbar/oauth2/kakao/dto/KakaoUserInfo.java
@@ -0,0 +1,26 @@
+package com.sossbar.oauth2.kakao.dto;
+
+import lombok.Data;
+
+@Data
+public class KakaoUserInfo {
+    private Long id;
+    private KakaoAccount kakao_account;
+    private Properties properties;
+
+    @Data
+    public static class KakaoAccount {
+        private String email;
+        private Profile profile;
+
+        @Data
+        public static class Profile {
+            private String profile_image_url;
+        }
+    }
+
+    @Data
+    public static class Properties {
+        private String nickname;
+    }
+}
